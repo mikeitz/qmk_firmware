5b63e0e942795683206b02b7a61e45753581aa83
Mike Itz <mike@itzes.com>
2019-10-19 13:21:20 -0400

    keymap as of 10-19

--- keyboards/tiller/keymaps/wider/keymap.c
+++ keyboards/tiller/keymaps/wider/keymap.c
@@ -16,40 +16,45 @@
 #include QMK_KEYBOARD_H
 
 // Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes { QMKBEST = SAFE_RANGE, QMKURL };
+enum custom_keycodes { QWERTY = SAFE_RANGE, COLEMAK };
 
-#define LAYER_NAV 2
-#define LAYER_NUM 1
+#define LAYER_QWERTY 0
+#define LAYER_COLEMAK 2
+
+#define LAYER_NAV 3
+#define LAYER_NUM 4
+#define LAYER_WIN 5
+#define LAYER_LAST 9
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(/* Base */
-      KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, XXXXXXX,
-      KC_BSPC, KC_A, KC_S, KC_D, KC_F, KC_G, XXXXXXX,
-      KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, XXXXXXX,
-      KC_LCTL, KC_LGUI, KC_LALT, KC_TAB, MO(LAYER_NAV), MO(LAYER_NUM), KC_LCTL,
+    [LAYER_QWERTY] = LAYOUT(/* Base */
+      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_ESC,
+      KC_BSPC, KC_A, KC_S, KC_D, KC_F, KC_G, LM(LAYER_WIN, MOD_LALT),
+      KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, LM(LAYER_WIN, MOD_LCTL),
+      KC_LCTL, KC_LGUI, KC_LALT, KC_LALT, MO(LAYER_NUM), MO(LAYER_NAV), KC_LCTL,
 
       XXXXXXX, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINUS,
-      XXXXXXX, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE,
-      XXXXXXX, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_BSLASH,
-      KC_ENT, KC_SPC, KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+      MO(LAYER_NAV), KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE,
+      MO(LAYER_NUM), KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, SFT_T(KC_BSLASH),
+      KC_ENT, KC_SPC, KC_TAB, KC_LGUI, XXXXXXX, XXXXXXX, MO(LAYER_NUM)
     ),
 
     [LAYER_NUM] = LAYOUT(
       KC_TILDE, KC_1, KC_2, KC_3, KC_4, KC_5, _______,
-      KC_DEL, KC_EXCLAIM, KC_AT, KC_HASH, KC_DOLLAR, KC_PERCENT, _______,
-      KC_GRAVE, _______, _______, KC_ENT, KC_SPC, KC_TAB, _______,
+      _______, KC_EXCLAIM, KC_AT, KC_HASH, KC_DOLLAR, KC_PERCENT, _______,
+      _______, KC_ENT, S(KC_TAB), KC_TAB, KC_SPC, KC_LALT, _______,
       _______, _______, _______, _______, _______, _______, _______,
 
       _______, KC_6, KC_7, KC_8, KC_9, KC_0, KC_PLUS,
       _______, KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_LPRN, KC_RPRN, KC_EQUAL,
-      _______, _______, KC_LBRACKET, KC_RBRACKET, KC_LCBR, KC_RCBR, KC_PIPE,
+      _______, KC_GRAVE, KC_LBRACKET, KC_RBRACKET, KC_LCBR, KC_RCBR, KC_PIPE,
       _______, _______, _______, _______, _______, _______, _______
     ),
 
     [LAYER_NAV] = LAYOUT(
-      A(F4), _______, C(KC_LEFT), KC_UP, C(KC_RIGHT), KC_HOME, _______, 
-      _______, KC_DEL, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END, _______,
-      _______, _______, _______, KC_ENT, KC_SPC, KC_TAB, _______,
+      KC_ESC, _______, C(KC_LEFT), KC_UP, C(KC_RIGHT), _______, A(KC_F4), 
+      KC_DEL, KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END, _______,
+      _______, KC_ENT, S(KC_TAB), KC_TAB, KC_SPC, KC_LALT, _______,
       _______, _______, _______, _______, _______, _______, _______,
 
       _______, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, _______,
@@ -58,7 +63,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, _______, _______, _______, _______, _______, _______
     ),
 
-    [9] = LAYOUT(
+    [LAYER_WIN] = LAYOUT(
+      _______, _______, _______, _______, S(KC_TAB), KC_TAB, _______,
+      _______, _______, _______, _______, S(KC_TAB), KC_TAB, _______,
+      _______, _______, _______, _______, S(KC_TAB), KC_TAB, _______,
+      _______, _______, _______, _______, _______, _______, _______,
+
+      _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______
+    ),
+
+    [LAYER_LAST] = LAYOUT(
       _______, _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______,
@@ -72,25 +89,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case QMKBEST:
-            if (record->event.pressed) {
-                // when keycode QMKBEST is pressed
-                SEND_STRING("QMK is the best thing ever!");
-            } else {
-                // when keycode QMKBEST is released
-            }
-            break;
-        case QMKURL:
-            if (record->event.pressed) {
-                // when keycode QMKURL is pressed
-                SEND_STRING("https://qmk.fm/" SS_TAP(X_ENTER));
-            } else {
-                // when keycode QMKURL is released
-            }
-            break;
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(LAYER_QWERTY);
+      }
+      return false;
+      break;
+    case COLEMAK:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(LAYER_COLEMAK);
+      }
+      return false;
+      break;
     }
-    return true;
+  return true;
 }
 
 void matrix_init_user(void) {}
