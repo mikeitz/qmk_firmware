7128c840c7132bf931b63490cffbc8132d1e9947
Mike Itz <mike@itzes.com>
2019-11-04 08:26:25 -0500

    adjustments to make ctl bsps and del possible

--- keyboards/tiller/keymaps/wider/keymap.c
+++ keyboards/tiller/keymaps/wider/keymap.c
@@ -46,23 +46,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_BASE] = LAYOUT(/* Base */
       KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, XXXXXXX,
       KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, XXXXXXX,
-      XXXXXXX, KC_Z, KC_X, KC_C, KC_V, KC_B, XXXXXXX,
+      TD(TD_SFT_CTL), KC_Z, KC_X, KC_C, KC_V, KC_B, XXXXXXX,
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TD(TD_ALT_GUI), SFT_T(KC_BSPC), CTL_T(KC_DEL),
 
       XXXXXXX, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINUS,
       XXXXXXX, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE,
       XXXXXXX, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, XXXXXXX,
-      LT(LAYER_NAV, KC_ENT), LT(LAYER_SYM, KC_SPC), MO(LAYER_NAV), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+      LT(LAYER_NAV, KC_ENT), LT(LAYER_SYM, KC_SPC), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
-    
+
     [LAYER_SYM] = LAYOUT(
       KC_TILDE, KC_EXCLAIM, KC_AT, KC_LCBR, KC_RCBR, KC_BSLASH, _______,
-      KC_GRAVE, KC_HASH, KC_DOLLAR, KC_LPRN, KC_RPRN, KC_PIPE, _______,
+      _______, KC_HASH, KC_DOLLAR, KC_LPRN, KC_RPRN, KC_PIPE, _______,
       _______, KC_PERCENT, KC_CIRCUMFLEX, KC_LBRACKET, KC_RBRACKET, KC_AMPERSAND, _______,
-      _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, KC_BSPC, KC_SPC,
 
       _______, KC_ASTERISK, KC_7, KC_8, KC_9, KC_EQUAL, _______,
-      _______, KC_PLUS, KC_4, KC_5, KC_6, KC_0, _______,
+      _______, KC_PLUS, KC_4, KC_5, KC_6, KC_0, KC_GRAVE,
       _______, KC_MINUS, KC_1, KC_2, KC_3, KC_DOT, _______,
       _______, _______, _______, _______, _______, _______, _______
     ),
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
       _______, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
       _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F12,
-      _______, TG(LAYER_GAME), _______, KC_LCTL, KC_LSFT, KC_LALT, _______, 
+      _______, TG(LAYER_GAME), _______, KC_LSFT, KC_LALT, KC_LCTL, _______,
       _______, _______, _______, _______, _______, _______, _______
     ),
 
