5b5f3d955fe86a5a7cca3b8fde3302ca9101164d
Mike Itz <mike@itzes.com>
2019-11-04 09:24:23 -0500

    move esc and tab to thumbs, add colemak layer

--- keyboards/tiller/keymaps/wider/keymap.c
+++ keyboards/tiller/keymaps/wider/keymap.c
@@ -34,6 +34,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 };
 
 #define LAYER_BASE 0
+#define LAYER_COLEMAK 1
 
 #define LAYER_NAV 3
 #define LAYER_SYM 4
@@ -43,16 +44,28 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [LAYER_BASE] = LAYOUT(/* Base */
+    [LAYER_BASE] = LAYOUT(
       KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, XXXXXXX,
       KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, XXXXXXX,
       TD(TD_SFT_CTL), KC_Z, KC_X, KC_C, KC_V, KC_B, XXXXXXX,
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TD(TD_ALT_GUI), SFT_T(KC_BSPC), CTL_T(KC_DEL),
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TD(TD_ALT_GUI), SFT_T(KC_BSPC), CTL_T(KC_TAB),
 
       XXXXXXX, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINUS,
       XXXXXXX, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE,
       XXXXXXX, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, XXXXXXX,
-      LT(LAYER_NAV, KC_ENT), LT(LAYER_SYM, KC_SPC), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+      LT(LAYER_NAV, KC_ENT), LT(LAYER_SYM, KC_SPC), KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    ),
+
+    [LAYER_COLEMAK] = LAYOUT(
+      _______, KC_Q, KC_W, KC_F, KC_P, KC_B, _______,
+      _______, KC_A, KC_R, KC_S, KC_T, KC_G, _______,
+      _______, KC_Z, KC_X, KC_C, KC_D, KC_V, _______,
+      _______, _______, _______, _______, _______, _______, _______,
+
+      _______, KC_J, KC_L, KC_U, KC_Y, KC_SCOLON, _______,
+      _______, KC_M, KC_N, KC_E, KC_I, KC_O, _______,
+      _______, KC_K, KC_H, KC_COMMA, KC_DOT, KC_SLASH, _______,
+      _______, _______, _______, _______, _______, _______, _______
     ),
 
     [LAYER_SYM] = LAYOUT(
@@ -71,12 +84,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, KC_HOME, C(KC_LEFT), KC_UP, C(KC_RIGHT), A(KC_F4), _______,
       _______, KC_LSFT, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END, _______,
       _______, S(KC_TAB), KC_TAB, KC_ENT, KC_SPC, _______, _______,
-      _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______,  CTL_T(KC_DEL),
 
 
       _______, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
       _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F12,
-      _______, TG(LAYER_GAME), _______, KC_LSFT, KC_LALT, KC_LCTL, _______,
+      _______, TG(LAYER_GAME), TG(LAYER_COLEMAK), KC_LSFT, KC_LALT, KC_LCTL, _______,
       _______, _______, _______, _______, _______, _______, _______
     ),
 
