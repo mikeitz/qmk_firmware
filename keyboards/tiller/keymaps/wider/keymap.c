2603422677c899136c95686e79724313f5972060
Mike Itz <mike@itzes.com>
2019-12-01 11:21:11 -0500

    rework to eliminate right thumb stress on shift

--- keyboards/tiller/keymaps/wider/keymap.c
+++ keyboards/tiller/keymaps/wider/keymap.c
@@ -17,7 +17,7 @@
 
 // -path:keyboards -path:users -path:layouts -path:docs
 
-enum custom_keycodes { ESC_STAB = SAFE_RANGE, };
+enum custom_keycodes { KC_STAB = SAFE_RANGE, };
 
 enum {
   TD_ALT_GUI = 0,
@@ -42,18 +42,20 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 #define LAYER_LAST 15
 
+bool numlock_on = false;
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [LAYER_BASE] = LAYOUT(
-      KC_EQUAL, KC_Q, KC_W, KC_E, KC_R, KC_T, XXXXXXX,
-      CTL_T(KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G, XXXXXXX,
-      ESC_STAB, KC_Z, KC_X, KC_C, KC_V, KC_B, XXXXXXX,
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TD(TD_ALT_GUI), LT(LAYER_NAV, KC_BSPC), CTL_T(KC_DEL),
+      KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, XXXXXXX,
+      LT(LAYER_NAV, KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G, XXXXXXX,
+      KC_STAB, KC_Z, KC_X, KC_C, KC_V, KC_B, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TD(TD_ALT_GUI), SFT_T(KC_BSPC), CTL_T(KC_DEL),
 
       XXXXXXX, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINUS,
-      XXXXXXX, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE,
-      XXXXXXX, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_UNDERSCORE,
-      SFT_T(KC_ENT), LT(LAYER_SYM, KC_SPC), MO(LAYER_FN), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX 
+      XXXXXXX, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, LT(LAYER_NAV, KC_QUOTE),
+      XXXXXXX, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_EQUAL,
+      LT(LAYER_LAST, KC_ENT), LT(LAYER_SYM, KC_SPC), MO(LAYER_FN), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX 
     ),
 
     [LAYER_COLEMAK] = LAYOUT(     _______, KC_Q, KC_W, KC_F, KC_P, KC_B, _______,
@@ -68,28 +70,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [LAYER_SYM] = LAYOUT(
-      _______, KC_HASH, KC_AT, KC_LCBR, KC_RCBR, KC_BSLASH, _______,
-      _______, KC_AMPERSAND, KC_PIPE, KC_LPRN, KC_RPRN, KC_EXCLAIM, _______,
-      _______, KC_PERCENT, KC_DOLLAR, KC_LBRACKET, KC_RBRACKET, _______, _______,
-      _______, _______, _______, _______, _______, KC_BSPC, KC_SPC,
+      KC_GRAVE, KC_EXCLAIM, KC_AT, KC_LCBR, KC_RCBR, KC_BSLASH, _______,
+      KC_TILDE, KC_HASH, KC_PIPE, KC_LPRN, KC_RPRN, KC_PIPE, _______,
+      _______, KC_PERCENT, KC_AMPERSAND, KC_LBRACKET, KC_RBRACKET, _______, _______,
+      _______, _______, _______, _______, _______, KC_SPC, KC_BSPC,
 
-      _______, KC_EQUAL, KC_7, KC_8, KC_9, KC_ASTERISK, _______,
-      _______, KC_MINUS, KC_4, KC_5, KC_6, KC_0, _______,
-      _______, KC_PLUS, KC_1, KC_2, KC_3, KC_DOT, _______,
+      _______, KC_CIRCUMFLEX, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_PLUS, KC_KP_MINUS,
+      _______, KC_KP_SLASH, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_0, KC_KP_ASTERISK,
+      _______, KC_EQUAL, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_DOT, KC_KP_ENTER,
       _______, _______, _______, _______, _______, _______, _______
     ),
 
     [LAYER_NAV] = LAYOUT(
-      KC_ESC, KC_ESC, KC_HOME, KC_UP, KC_END, _______, _______,
-      _______, C(KC_LEFT), KC_LEFT, KC_DOWN, KC_RIGHT, C(KC_RIGHT), _______,
-      _______, KC_ENT, _______, _______, KC_SPC, _______, _______,
-      _______, _______, _______, _______, _______, _______,  _______,
+      _______, _______, C(KC_LEFT), KC_UP, C(KC_RIGHT), _______, _______,
+      _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END, _______,
+      _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, KC_ENT, C(KC_BSPC),  C(KC_DEL),
 
 
-      _______, KC_CIRCUMFLEX, KC_EQUAL, KC_ASTERISK, KC_TILDE, KC_GRAVE, _______,
-      _______, KC_BSPC, KC_MINUS, KC_UNDERSCORE, KC_DQUO, KC_QUOTE, _______,
-      _______, KC_DEL, KC_PLUS, KC_RSFT, KC_RALT, KC_RCTL, KC_RSFT,
-      _______, CTL_T(KC_SPC), KC_LALT, _______, _______, _______, _______
+      _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, KC_RCTL, KC_RSFT, KC_RALT, KC_RWIN, _______,
+      _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______
     ),
 
     [LAYER_FN] = LAYOUT(
@@ -101,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
       _______, KC_F10, KC_F7, KC_F8, KC_F9, _______, _______,
       _______, KC_F11, KC_F4, KC_F5, KC_F6, KC_LALT, _______,
-      _______, KC_F12, KC_F1, KC_F2, KC_F3, _______, _______,
+      _______, KC_F12, KC_F1, KC_F2, KC_F3, KC_LCTL, KC_LSFT,
       _______, _______, _______, _______, _______, _______, _______
     ),
 
@@ -113,20 +115,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
       _______, _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, MO(LAYER_SYM),
       _______, MO(LAYER_NAV), _______, _______, _______, _______, _______
     ),
 
     [LAYER_LAST] = LAYOUT(
-      _______, _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______, _______,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 
-      _______, _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______, _______
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     )
 };
 
@@ -135,9 +137,14 @@ static uint16_t esc_stab_timer = 0;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   bool interrupted = last_keycode != keycode;
-  last_keycode = keycode;
+  if (record->event.pressed) {
+    last_keycode = keycode;
+  }
+  if (!numlock_on) {
+    register_code(KC_NLCK);
+  }
   switch (keycode) {
-    case ESC_STAB:
+    case KC_STAB:
       if (record->event.pressed) {
         esc_stab_timer = timer_read();
         register_code(KC_LSFT);
@@ -162,4 +169,10 @@ void matrix_init_user(void) {}
 
 void matrix_scan_user(void) {}
 
-void led_set_user(uint8_t usb_led) {}
+void led_set_user(uint8_t usb_led) {
+  if (usb_led & (1<<USB_LED_NUM_LOCK)) {
+    numlock_on = true;
+  } else {
+    numlock_on = false;
+  }
+}
