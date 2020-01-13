33801f32d47aad9d4e059e1fc4de77d5024c46d6
Mike Itz <mike@itzes.com>
2020-01-13 18:04:01 -0500

    fn toggle

--- keyboards/crkbd/keymaps/mitz/keymap.c
+++ keyboards/crkbd/keymaps/mitz/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       LT(LAYER_NAV, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G,       KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE,
       KC_LGUI, KC_Z, KC_X, KC_C, KC_V, KC_B,      KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_BSLASH,
       LM(LAYER_ALT, MOD_LALT), KC_SFT_BSPC, KC_CTL_DEL,
-      LT(LAYER_NAV, KC_ENT), LT(LAYER_SYM, KC_SPC), XXXXXXX
+      LT(LAYER_NAV, KC_ENT), LT(LAYER_SYM, KC_SPC), MO(LAYER_FN)
     ),
 
     [LAYER_CM] = LAYOUT(
@@ -87,9 +87,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [LAYER_FN] = LAYOUT(
-      _______, _______, _______, _______, _______, _______,      KC_F10, KC_F7, KC_F8, KC_F9, _______, _______,
-      _______, _______, _______, _______, _______, _______,      KC_F11, KC_F4, KC_F5, KC_F6, _______, _______,
-      _______, _______, _______, _______, _______, _______,      KC_F12, KC_F1, KC_F2, KC_F3, _______, _______,
+      _______, _______, _______, _______, _______, _______,      _______, KC_F7, KC_F8, KC_F9, KC_F11, _______,
+      _______, _______, _______, _______, _______, _______,      _______, KC_F4, KC_F5, KC_F6, KC_F10, _______,
+      _______, _______, _______, _______, _______, _______,      _______, KC_F1, KC_F2, KC_F3, KC_F12, _______,
       _______, _______, _______,
       _______, _______, _______
     )
