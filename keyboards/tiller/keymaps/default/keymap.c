ae8fe3f97763839ce67a63d94bd3f3d76819fab8
Mike Itz <mike@itzes.com>
2020-01-13 20:23:11 -0500

    force symbol layer

--- keyboards/tiller/keymaps/default/keymap.c
+++ keyboards/tiller/keymaps/default/keymap.c
@@ -47,14 +47,14 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [LAYER_BASE] = LAYOUT(
-      KC_EQUAL, KC_Q, KC_W, KC_E, KC_R, KC_T,
+      XXXXXXX, KC_Q, KC_W, KC_E, KC_R, KC_T,
       LT(LAYER_NAV, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G,
       MO(LAYER_MUS), KC_Z, KC_X, KC_C, KC_V, KC_B,
       LM(LAYER_ALT, MOD_LALT), KC_SFT_BSPC, KC_CTL_DEL,
 
-      KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINUS,
-      KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE,
-      KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_BSLASH,
+      KC_Y, KC_U, KC_I, KC_O, KC_P, XXXXXXX,
+      KC_H, KC_J, KC_K, KC_L, KC_SCOLON, MO(LAYER_NAV),
+      KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, XXXXXXX,
       LT(LAYER_NAV, KC_ENT), LT(LAYER_SYM, KC_SPC), XXXXXXX
     ),
 
@@ -92,9 +92,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LGUI, _______, _______, _______, _______, _______,
       _______, _______, _______,
 
-      _______, KC_7, KC_8, KC_9, KC_MINUS, _______,
-      _______, KC_4, KC_5, KC_6, KC_0, _______,
-      _______, KC_1, KC_2, KC_3, KC_DOT, _______,
+      KC_PLUS, KC_7, KC_8, KC_9, KC_MINUS, _______,
+      KC_EQUAL, KC_4, KC_5, KC_6, KC_0, _______,
+      KC_SLASH, KC_1, KC_2, KC_3, KC_DOT, _______,
       _______, _______, _______
     ),
 
