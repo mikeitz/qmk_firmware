66f925151a85b5ee64c0896976fa762430802387
Mike Itz <mike@itzes.com>
2020-02-06 21:07:51 -0500

    also update crkbd

--- keyboards/tiller/keymaps/simpler/keymap.c
+++ keyboards/tiller/keymaps/simpler/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, KC_EXCLAIM, KC_AT, KC_LCBR, KC_RCBR, KC_PERCENT,
       _______, KC_HASH, KC_DOLLAR, KC_LPRN, KC_RPRN, XXXXXXX,
       _______, KC_TILDE, KC_GRAVE, KC_LBRACKET, KC_RBRACKET, XXXXXXX,
-      _______, _______, _______,
+      _______, _______, KC_BSPC,
 
       KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_EQUAL, KC_PLUS, KC_DEL,
       XXXXXXX, KC_DQUO, KC_QUOTE, KC_MINUS, KC_UNDERSCORE, _______,
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ASTERISK, KC_7, KC_8, KC_9, KC_PLUS, _______,
       KC_MINUS, KC_4, KC_5, KC_6, KC_0, _______,
       KC_SLASH, KC_1, KC_2, KC_3, KC_DOT, _______,
-      _______, _______, _______
+      KC_TAB, _______, _______
     ),
 
     [LAYER_FN] = LAYOUT(
