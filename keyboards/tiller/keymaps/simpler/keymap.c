ed3bbf769f97964202154fbf44f2ec54d38e98b2
Mike Itz <mike@itzes.com>
2020-02-13 18:09:04 -0500

    try tab on the left side

--- keyboards/tiller/keymaps/simpler/keymap.c
+++ keyboards/tiller/keymaps/simpler/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_NAV] = LAYOUT(
       _______, XXXXXXX, C(KC_LEFT), KC_UP, C(KC_RIGHT), KC_HOME,
       CTL_T(KC_ENT), KC_LSFT, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END,
-      _______, XXXXXXX, XXXXXXX, KC_PGUP, KC_PGDN, XXXXXXX,
+      _______, KC_TAB, S(KC_TAB), KC_PGUP, KC_PGDN, XXXXXXX,
       _______, _______, _______,
 
       KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_EQUAL, KC_PLUS, _______,
