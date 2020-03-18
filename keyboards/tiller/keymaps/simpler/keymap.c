0154620638e677a93cddf8bb2b69328bb097c952
Mike Itz <mike@itzes.com>
2020-03-18 16:17:57 -0400

    swap back esc/tab

--- keyboards/tiller/keymaps/simpler/keymap.c
+++ keyboards/tiller/keymaps/simpler/keymap.c
@@ -38,8 +38,8 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [LAYER_BASE] = LAYOUT(
-      KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T,
-      CTL_T(KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G,
+      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,
+      CTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G,
       KC_LALT, KC_Z, KC_X, KC_C, KC_V, KC_B,
       KC_LGUI, KC_LSFT, MO(LAYER_NAV),
 
