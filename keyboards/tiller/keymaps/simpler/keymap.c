c47f6f08391d5cb311cfee17428ad443aca6a79a
Mike Itz <mike@itzes.com>
2020-08-31 00:27:37 -0400

    avoid modtap on tab layer

--- keyboards/tiller/keymaps/simpler/keymap.c
+++ keyboards/tiller/keymaps/simpler/keymap.c
@@ -32,6 +32,7 @@ enum custom_keycodes {
 #define LAYER_GAME 3
 #define LAYER_GAME2 4
 #define LAYER_GAME3 5
+
 #define LAYER_NAV 12
 #define LAYER_SYM 13
 #define LAYER_FN 14
@@ -43,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,   
       KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,
       KC_LALT, KC_Z, KC_X, KC_C, KC_V, KC_B,
-      KC_LGUI, KC_LSFT, MO(LAYER_NAV),
+      KC_LGUI, KC_LSFT, TT(LAYER_NAV),
 
       KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
       KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_ENT,
@@ -60,14 +61,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_EQUAL_US, KC_PLUS, _______,
       XXXXXXX, KC_DQUO, KC_QUOTE_US, KC_MINUS_US, KC_UNDERSCORE, _______,
       XXXXXXX, KC_PIPE, KC_LT, KC_GT, KC_BSLASH_US, _______,
-      _______, _______, _______   
+      _______, _______, _______
     ),
 
     [LAYER_NAV] = LAYOUT( 
       KC_ESC, A(KC_F4), C(KC_LEFT), KC_UP, C(KC_RIGHT), KC_PGUP,
       _______, KC_LSFT, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN,
       _______, TT(LAYER_MUS), KC_SPC, KC_HOME, KC_END, KC_DEL,
-      _______, _______, _______,
+      _______, _______, TG(LAYER_NAV),
 
       XXXXXXX, KC_7, KC_8, KC_9, KC_MINUS, _______,
       KC_TAB, KC_4, KC_5, KC_6, KC_0, _______,
@@ -103,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______,
-      _______, SFT_T(KC_TAB), S(KC_TAB),
+      S(KC_TAB), _______, KC_TAB,
 
       _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______,
