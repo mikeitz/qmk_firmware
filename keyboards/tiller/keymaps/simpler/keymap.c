b254b3529fa0e7612fbe39b94e8ad57fb743ef0f
Mike Itz <mike@itzes.com>
2020-11-25 09:56:44 -0500

    swap left hand delete and alt-f4 keys to avoid accidental game exits when accidentally not in game mode

--- keyboards/tiller/keymaps/simpler/keymap.c
+++ keyboards/tiller/keymaps/simpler/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,   
       KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,
       KC_LALT, KC_Z, KC_X, KC_C, KC_V, KC_B,
-      KC_LGUI, KC_LSFT, TT(LAYER_NAV),
+      KC_LGUI, KC_LSFT, MO(LAYER_NAV),
 
       KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
       KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_ENT,
@@ -65,9 +65,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [LAYER_NAV] = LAYOUT( 
-      KC_ESC, A(KC_F4), C(KC_LEFT), KC_UP, C(KC_RIGHT), KC_PGUP,
+      KC_ESC, KC_DEL, C(KC_LEFT), KC_UP, C(KC_RIGHT), KC_PGUP,
       _______, KC_LSFT, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN,
-      _______, TT(LAYER_MUS), KC_HOME, KC_SPC, KC_END, KC_DEL,
+      _______, TT(LAYER_MUS), KC_HOME, KC_SPC, KC_END, A(KC_F4),
       _______, _______, TG(LAYER_NAV),
 
       XXXXXXX, KC_7, KC_8, KC_9, KC_MINUS, _______,
