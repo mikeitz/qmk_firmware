a4ec93b39a84fdb60556f36bd48d9ab3f20541eb
Mike Itz <mike@itzes.com>
2020-11-26 19:53:01 -0500

    put a-f4 back where it was, and try putting undo on a different left hand nav key

--- keyboards/tiller/keymaps/simpler/keymap.c
+++ keyboards/tiller/keymaps/simpler/keymap.c
@@ -65,14 +65,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [LAYER_NAV] = LAYOUT( 
-      KC_ESC, KC_DEL, C(KC_LEFT), KC_UP, C(KC_RIGHT), KC_PGUP,
+      KC_ESC, A(KC_F4), C(KC_LEFT), KC_UP, C(KC_RIGHT), KC_PGUP,
       _______, KC_LSFT, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN,
-      _______, TT(LAYER_MUS), KC_HOME, KC_SPC, KC_END, A(KC_F4),
+      _______, TT(LAYER_MUS), KC_HOME, KC_SPC, KC_END, C(KC_Z),
       _______, _______, TG(LAYER_NAV),
 
       C(KC_Y), KC_7, KC_8, KC_9, KC_MINUS, _______,
-      C(KC_Z), KC_4, KC_5, KC_6, KC_0, _______,
-      C(KC_A), KC_1, KC_2, KC_3, KC_DOT, _______,
+      XXXXXXX, KC_4, KC_5, KC_6, KC_0, _______,
+      XXXXXXX, KC_1, KC_2, KC_3, KC_DOT, _______,
       LT(LAYER_SYM, KC_TAB), _______, S(KC_TAB)
     ),
 
