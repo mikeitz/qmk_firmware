6bb74f1c69df3e7f551ba4a0664fe0172c835db1
Mike Itz <mike@itzes.com>
2020-11-29 23:58:43 -0500

    ditch alternate fn approach and instead try to make the arrow half of the nav layer easier to use mods on

--- keyboards/tiller/keymaps/simpler/keymap.c
+++ keyboards/tiller/keymaps/simpler/keymap.c
@@ -67,28 +67,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_NAV] = LAYOUT( 
       KC_ESC, A(KC_F4), C(KC_LEFT), KC_UP, C(KC_RIGHT), KC_PGUP,
       _______, KC_LSFT, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN,
-      _______, TT(LAYER_MUS), KC_HOME, KC_SPC, KC_END, C(KC_Z),
-      _______, _______, TG(LAYER_NAV),
+      MO(LAYER_MUS), KC_LALT, KC_LCTL, KC_HOME, KC_END, KC_SPC,
+      _______, _______, _______,
 
       C(KC_Y), KC_7, KC_8, KC_9, KC_MINUS, _______,
       XXXXXXX, KC_4, KC_5, KC_6, KC_0, _______,
       XXXXXXX, KC_1, KC_2, KC_3, KC_DOT, _______,
-      LT(LAYER_SYM, KC_TAB), _______, S(KC_TAB)
+      _______, _______, KC_LALT
     ),
 
-    [LAYER_FN] = LAYOUT(
-      XXXXXXX, KC_F11, KC_F7, KC_F8, KC_F9, TG(LAYER_GAME2),
-      _______, KC_F10, KC_F4, KC_F5, KC_F6, TG(LAYER_GAME),
-      _______, KC_F12, KC_F1, KC_F2, KC_F3, TG(LAYER_MUS),
-      _______, _______, XXXXXXX,
-
-      KC_SCROLLLOCK, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SYSREQ,
-      KC_CAPSLOCK, KC_HOME, KC_UP, KC_END, XXXXXXX, KC_PSCREEN,
-      KC_NUMLOCK, KC_LEFT, KC_DOWN, KC_RIGHT, KC_LALT, KC_INSERT,
-      XXXXXXX, XXXXXXX, _______
-    ),
-
-    /*
     [LAYER_FN] = LAYOUT(
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TG(LAYER_GAME2),
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TG(LAYER_GAME),
@@ -100,7 +87,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_NUMLOCK, KC_F1, KC_F2, KC_F3, KC_F12, KC_INSERT,
       XXXXXXX, XXXXXXX, _______
     ),
-    */
 
     [LAYER_MUS] = LAYOUT(
       TG(LAYER_MUS), KC_OCT_0, KC_OCT_1, KC_OCT_2, KC_OCT_3, KC_OCT_4,
